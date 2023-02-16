ab3d3a7b2ef56068071110a396f5b9d9cd05a14b
Simshon <simshon.horie@gmail.com>
2023-02-15 22:39:39 -0300

    Projeto desenvolvido em curso

---
+++ js/remover-paciente.js
@@ -0,0 +1,7 @@
+var tabela = document.querySelector('#tabela-pacientes');
+adicionaFuncaoRemove(tabela);
+/*
+
+for(var i = 0; i < listaDePacientes.length; i++) {
+    adicionaFuncaoRemove(listaDePacientes[i]);
+}*/
